d363b1eeea11608aeeac5a3ee6fb609ae69983c1
ayoublos <abelibel@pursuit.org>
2024-07-02 19:04:16 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div>Hello World</div>;
+  const API=import.meta.env.VITE_API_URL
+  return (
+<div>
+  <h1>
+    Hello World
+  </h1>
+  {/* <Router>
+    <NavBar/>
+
+  </Router> */}
+</div>
+
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,6 @@
+function NavBar(){
+    return (<div className="navbar-container">
+
+    </div>)
+}
+export default NavBar;
